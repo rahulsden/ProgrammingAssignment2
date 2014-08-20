ec0dae0c737e63574a38c180ab8edeef01af7212
Rahul Gupta <rahulgupta999@gmail.com>
2014-08-20 19:58:11 +0530

    changes for PA2 in R Programming course, added cacheMatrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    xinverse <- NULL
+    set <- function(y) {
+        x <<- y
+        xinverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(minverse) xinverse <<- minverse
+    getinverse <- function() xinverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+## Computes the inverse. If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    minverse <- x$getinverse()
+    if (!is.null(minverse)) {
+        message("returning cached data")
+        return(minverse)
+    }
+    m <- x$get()
+    minverse <- solve(m)
+    x$setinverse(minverse)
+    minverse
 }
